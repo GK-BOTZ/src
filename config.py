7835cd871cc0d174f1ba75fc8c745100ae0e5c49
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2024-10-15 00:11:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", 20747302))
 API_HASH = getenv("API_HASH", "6e086ad99a197709af10425d7c7c1b65")
-BOT_TOKEN = getenv("BOT_TOKEN", "7944884364:AAF23zcZ2EokJeSkVL_roMNN9duJqVCLryk")
+BOT_TOKEN = getenv("BOT_TOKEN", "7287641896:AAH87qnrE7tWN1B1QvXIrtfa4FaH12Iogjg")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6805001741").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://gautamkumar83226:NHKzaiNLlzzedv69@cluster0.mbaodme.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP = getenv("LOG_GROUP", "-1002156222847")
