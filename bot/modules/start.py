caf469304c55f05b7884c11165024710ccafb10a
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2024-10-16 23:58:16 +0530

    Update start.py

--- bot/modules/start.py
+++ bot/modules/start.py
@@ -4,11 +4,13 @@ from bot.core.func import handle_force_sub, srm
 from config import OWNER_ID, CHANNEL_LINK, OWNER
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import user_locks, global_lock
+import asyncio
+import config
 
 
-@bot.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def start(c, m):
-    uid = m.from_user.id
+   uid = m.from_user.id
    async with global_lock:
       if uid not in user_locks:
          user_locks[uid] = asyncio.Lock()
