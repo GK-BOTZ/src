933b2485fae353bb652fd6dea354913f1901f88c
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2024-10-16 23:55:28 +0530

    FSub

--- bot/modules/start.py
+++ bot/modules/start.py
@@ -1,24 +1,21 @@
-from pyrogram import filters
-from bot import bot
+from pyrogram import filters, Client
 from bot.core import script
-from bot.core.func import subscribe
-from config import OWNER_ID
+from bot.core.func import handle_force_sub, srm
+from config import OWNER_ID, CHANNEL_LINK, OWNER
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from bot import user_locks, global_lock
 
-# ------------------- Start-Buttons ------------------- #
-
-buttons = InlineKeyboardMarkup(
-    [
-        [InlineKeyboardButton("Join Channel", url="https://t.me/gk")],
-        [InlineKeyboardButton("Buy Premium", url="https://t.me/kingofpatal")]
-    ]
-)
 
 @bot.on_message(filters.command("start"))
-async def start(_, message):
-    join = await subscribe(_, message)
-    if join == 1:
-        return
-    await message.reply_photo(photo="https://graph.org/file/4e80dc2f4f6f2ddadb4d2.jpg",
-                              caption=script.START_TXT.format(message.from_user.mention), 
-                              reply_markup=buttons)
+async def start(c, m):
+    uid = m.from_user.id
+   async with global_lock:
+      if uid not in user_locks:
+         user_locks[uid] = asyncio.Lock()
+            
+   async with user_locks[uid]:
+      if Config.FSUB_CHANNELS:
+         text, buttons = await handle_force_sub(c, m)
+         if buttons:
+            return await srm(c, m, text, markup=buttons, dt=100, photo="https://envs.sh/s/8w3GxIy7MKqojoEKWLu1IQ/SI1.jpg") 
+         await srm(c, m, photo="https://envs.sh/s/8w3GxIy7MKqojoEKWLu1IQ/SI1.jpg", text=script.START_TXT.format(m.from_user.mention), markup=buttons)
